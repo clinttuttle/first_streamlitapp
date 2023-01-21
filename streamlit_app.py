1d7d4f62c777f5d04b9865074fd67ae7997c6a24
Clint Tuttle <clinttuttle@users.noreply.github.com>
2023-01-21 07:20:41 -0600

    Update streamlit_app.py
    remove raw json line

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())  #Just writes data to the screen
 
 # Uses pandas json normalize code to normalize json 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
