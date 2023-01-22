fae766ef8ef2fd47ba303c093e8cd5aa60e09141
Clint Tuttle <clinttuttle@users.noreply.github.com>
2023-01-21 23:12:37 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-#creat a function
+#create a function
 def get_fruitvice_data(this_fruit_choice):
       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
@@ -64,7 +64,7 @@ streamlit.stop()
 #Allow user to add fruit to the list
 def insert_row_snowflake(new_fruit):
       with my_cnx.cursor() as my_cur:
-            my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
+            my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('" + fruit_choice + "')")
             return "Thanks for adding " + new_fruit
       
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
